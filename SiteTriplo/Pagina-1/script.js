30d48b79e7ea5c32ab6fbf4e527d8e2cc39365ae
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-05-06 18:28:23 -0300

    Add files via upload

--- SiteTriplo/Pagina-1/script.js
+++ SiteTriplo/Pagina-1/script.js
@@ -1,16 +1,23 @@
 const sidebar = document.querySelector('aside');
 const buttonSidebar = document.querySelector('.sidebar-icon');
 const closeSidebar = document.querySelector('.sidebar-close-icon');
+const hrAside = document.querySelector('.hr-aside')
 
 
 buttonSidebar.addEventListener('click', () => {
     sidebar.classList.add('show');
+    hrAside.classList.add('show');
+    sidebar.children.item(2).children.item(0).classList.add('show')
+    sidebar.children.item(2).children.item(1).classList.add('show')
     buttonSidebar.classList.add('active');
     closeSidebar.classList.add('active');
 });
 
 closeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('show');
+    hrAside.classList.remove('show');
+    sidebar.children.item(2).children.item(0).classList.remove('show')
+    sidebar.children.item(2).children.item(1).classList.remove('show')
     buttonSidebar.classList.remove('active');
     closeSidebar.classList.remove('active');
 });
